362f9f10cf939c47401ddddd795800e13c8a21c5
Desmond Lee <desmondleejj@hotmail.com>
2021-03-13 16:29:46 +0800

    Add user controller

---
+++ BillTracker/Controllers/UserController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BillTracker.Models;
+
+namespace BillTracker.Controllers
+{
+    public class UserController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: User
+        public ActionResult Index()
+        {
+            var billModels = db.Bill.Include(b => b.User);
+            return View(billModels.ToList());
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BillModels billModels = db.Bill.Find(id);
+            if (billModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(billModels);
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BillId,BillName,BillCompany,BillDate,BillPrice,UserId")] BillModels billModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bill.Add(billModels);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", billModels.UserId);
+            return View(billModels);
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BillModels billModels = db.Bill.Find(id);
+            if (billModels == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", billModels.UserId);
+            return View(billModels);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BillId,BillName,BillCompany,BillDate,BillPrice,UserId")] BillModels billModels)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(billModels).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", billModels.UserId);
+            return View(billModels);
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BillModels billModels = db.Bill.Find(id);
+            if (billModels == null)
+            {
+                return HttpNotFound();
+            }
+            return View(billModels);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BillModels billModels = db.Bill.Find(id);
+            db.Bill.Remove(billModels);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
